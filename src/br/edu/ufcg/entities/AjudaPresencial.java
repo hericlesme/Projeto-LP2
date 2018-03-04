dac5b93ef76f0c300e4826bbc5c7d0d38841bfc6
Eduardo Cavalcanti <eduardo.freitas@ccc.ufcg.edu.br>
2018-03-03 22:29:48 -0300

    Adicionado 'DIA' ao enum de InfoTutor e ao construtor de AjudaPresencial

--- src/br/edu/ufcg/entities/AjudaPresencial.java
+++ src/br/edu/ufcg/entities/AjudaPresencial.java
@@ -4,13 +4,13 @@ public class AjudaPresencial extends Ajuda {
 
 	private String localInteresse;
 	private String horario;
+	private String dia;
 
-	public AjudaPresencial(String matrAluno, String disciplina, String horario,
-	        String localInteresse) {
-
+	public AjudaPresencial(String matrAluno, String disciplina, String horario, String dia, String localInteresse) {
 		super(matrAluno, disciplina);
 		this.localInteresse = localInteresse;
 		this.horario = horario;
+		this.disciplina = dia;
 	}
 
 	@Override
@@ -26,6 +26,8 @@ public class AjudaPresencial extends Ajuda {
 			return this.matrTutor;
 		case HORARIO:
 			return this.horario;
+		case DIA:
+			return this.dia;
 		default:
 			throw new IllegalArgumentException("nao man");
 		}
--- src/br/edu/ufcg/entities/InfoTutor.java
+++ src/br/edu/ufcg/entities/InfoTutor.java
@@ -1,5 +1,5 @@
 package br.edu.ufcg.entities;
 
 public enum InfoTutor {
-	MATR_TUTOR, MATR_ALUNO, LOCAL, HORARIO, DISCIPLINA, NOTA;
+	MATR_TUTOR, MATR_ALUNO, LOCAL, HORARIO, DISCIPLINA, NOTA, DIA;
 }
