bca4e90e2711294167bdb11af921c7b19f8bb17b
Henrique Castro Arriel <Henrique.arriel@ccc.ufcg.edu.br>
2018-03-13 18:43:35 -0300

    Fix doc facade + doc Exception

--- src/br/edu/ufcg/exceptions/AlreadyAssessedException.java
+++ src/br/edu/ufcg/exceptions/AlreadyAssessedException.java
@@ -1,13 +1,30 @@
 package br.edu.ufcg.exceptions;
 
+/**
+ * Classe que lança uma exceção. Herda de RuntimeException. É lançada quando uma
+ * Ajuda ja foi avaliada.
+ * 
+ * Projeto de Laboratório - Programação II.
+ *
+ */
+
 public class AlreadyAssessedException extends RuntimeException {
 
 	private static final long serialVersionUID = 5752752996459804914L;
 
+	/**
+	 * Lança uma exceção com mensagem a ser exibida.
+	 * 
+	 * @param s
+	 *                mensagem exibida.
+	 */
 	public AlreadyAssessedException(String s) {
 		super(s);
 	}
 
+	/**
+	 * Lança uma exceção sem mensagem.
+	 */
 	public AlreadyAssessedException() {
 		super();
 	}
